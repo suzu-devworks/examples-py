85d71d3445a12c68700d620b301a0aa872d49fe1
A.suzuki <suzu.devworks@gmail.com>
2021-01-04 17:56:37 +0900

    Add move photo script for PNG,GIF.

--- scripts/move-photo-files/move-other-image-files.py
+++ scripts/move-photo-files/move-other-image-files.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""Move photo files.
+"""Move other image files.
 
-This script is organize and move photo files into folders.
+This script is organize and move image files into folders.
 
-1. List jpeg files
-2. Get the shooting date from EXIF
+1. List image files (jpeg,png,gif)
+2. Get the file update date
 3. Create a subdirectory from the date of shooting
-     (current)/2019/2019-05/
+     (current)/2019/2019-nodate/
 4. Move file (make it without subdirectory)
 ...repeat
 """
@@ -17,9 +17,6 @@ import shutil
 from datetime import datetime
 from logging import getLogger
 from pathlib import Path
-
-from PIL import Image
-from PIL.ExifTags import TAGS
 
 LOGGING_CONFIG = {
     'version': 1,
@@ -53,55 +50,17 @@ logging.config.dictConfig(LOGGING_CONFIG)
 logger = getLogger(__name__)
 
 
-def find_photo_files(dir: str):
-    """Finds photo files (.jpg|.jpeg) from specified directory.
+def find_image_files(dir: str):
+    """Finds image files (.jpg|.jpeg|.png|gif) from specified directory.
 
     Args:
         dir: Source Directory in photo files.
     Yields:
         Path: Path instance of photo file .
     """
-    ext_matcher = re.compile('.*\\.jpe?g\\Z', re.IGNORECASE)
+    ext_matcher = re.compile('.*\\.(jpe?g|png|gif)\\Z', re.IGNORECASE)
     for f in (d for d in Path(dir).glob('**/*') if (d.match('[!\\.]*/*') and ext_matcher.match(d.name))):
         yield f
-
-
-def do_operation_image(path: str, func):
-    """Do operation wrapper for image instance.
-
-    Args:
-        path: Image file path.
-        func: Function of operation.
-    Returns:
-        any: operation result.
-    """
-    img = Image.open(path)
-    result = func(img)
-    img.close()
-    return result
-
-
-def get_exif_shooting_date(img: Image):
-    """Gets EXIF shooting date from image.
-
-    Args:
-        img : Image instance of photo.
-
-    Returns: 
-        datetime: EXIF shooting date (DateTimeOriginal).
-    """
-    exif = img._getexif()
-    try:
-        for id, val in exif.items():
-            tg = TAGS.get(id, id)
-            if tg == 'DateTimeOriginal':
-                dt = datetime.strptime(val, '%Y:%m:%d %H:%M:%S')
-                return dt
-
-    except (AttributeError, ValueError) as e:
-        logger.warning(repr(e))
-
-    return None
 
 
 def move_file(source: Path, dest: Path):
@@ -112,7 +71,7 @@ def move_file(source: Path, dest: Path):
         dest: Destination directory path
     """
     dest.mkdir(mode=0o777, parents=True, exist_ok=True)
-
+    
     try:
         #shutil.copy2(str(source), str(dest))
         #shutil.move(str(source), str(dest))
@@ -140,14 +99,9 @@ def main():
         logger.info('src_path:  {}'.format(source_path))
         logger.info('dest_path: {}'.format(dest_path_root))
 
-        for path in find_photo_files(source_path):
-            date_of_shooting = do_operation_image(path, get_exif_shooting_date)
-            if not isinstance(date_of_shooting, datetime):
-                logger.warning(
-                    'can not get the shooting date of EXIF: {}'.format(path))
-                continue
-
-            dest_path = dest_path_root / date_of_shooting.strftime('%Y/%Y-%m')
+        for path in find_image_files(source_path):
+            update_time = datetime.fromtimestamp(path.stat().st_mtime)
+            dest_path = dest_path_root / update_time.strftime('%Y/%Y-nodate')
 
             if isinstance(dest_path, Path):
                 move_file(path, dest_path)
