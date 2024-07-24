c0afb97bfbb35ee12ce9e3e062a118c6994eb990
A.suzuki <suzu.devworks@gmail.com>
2024-07-24 14:27:09 +0000

    Add argparse console example.

--- src/examples-py/src/examples/console/command.py
+++ src/examples-py/src/examples/console/command.py
@@ -1,20 +1,49 @@
+from argparse import ArgumentParser, Namespace, RawTextHelpFormatter
 from logging import getLogger
 
-from examples.libraries.logging import configure_logging, do_logging_example
+from examples.libraries.logging import configure_logging
+from examples.libraries.logging.example import do_logging_example
 
 configure_logging()
+logger = getLogger("examples.console.command")
+
+
+def _parse_arguments() -> Namespace:
+    parser = ArgumentParser(
+        description="console examples for argparse.",
+        formatter_class=RawTextHelpFormatter,
+    )
+
+    subparsers = parser.add_subparsers(
+        title="sub commands",
+        description="for examples commands",
+        help="choose command",
+        required=True,
+    )
+
+    # logging
+    logging_parser = subparsers.add_parser(
+        "logging",
+        help="logging example",
+        description="standard library/logging example",
+    )
+    logging_parser.set_defaults(exec=lambda args: do_logging_example())
+
+    args = parser.parse_args()
+
+    return args
 
 
 def main() -> None:
-    logger = getLogger("examples.console.command")
-    logger.info("start")
+    args = _parse_arguments()
+    logger.info("#start")
 
     try:
-        do_logging_example()
+        args.exec(args)
     except Exception:
         logger.exception("Exiting due to an unhandled exception.")
 
-    logger.info("end")
+    logger.info("#end")
 
 
 if __name__ == "__main__":
